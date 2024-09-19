1b3f4a8cab5c2d0967eae7af0017212e2921cebb
dictateur <joelpromero140@gmail.com>
2024-09-19 17:26:24 +0200

    Test.java: HashMap para gestionar users i credenciales

--- src/main/java/com/mycompany/test/Test.java
+++ src/main/java/com/mycompany/test/Test.java
@@ -4,10 +4,7 @@
 
 package com.mycompany.test;
 
-import java.lang.ProcessBuilder.Redirect.Type;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -15,17 +12,18 @@ import java.util.Scanner;
  * @author Dicta
  */
 public class Test {
-
+    static private String separacion = "------------------------------------";
     public static void main(String[] args) {
-        var separacion = "------------------------------------";
         Scanner scanner = new Scanner(System.in);
         int option;
         
-        ArrayList<String> user_names= new ArrayList<>(Arrays.asList("Joel", "Manu"));
-        
+        HashMap<String, String> user_credentials = new HashMap<>();
+        user_credentials.put("Joel", "1234");  // Joel tiene la contraseña 1234
+        user_credentials.put("Manu", "5678");  // Manu tiene la contraseña 5678
         
         do {
             System.out.println("Pulsa 1 para iniciar sesion:");
+            System.out.println("Pulsa 2 para mostrar credenciales (Test):");
             System.out.println("Pulsa 3 para cerrar:");
             System.out.println(separacion);
 
@@ -34,12 +32,13 @@ public class Test {
 
             switch (option) {
                 case 1:
-                    System.out.println("Introduce nombre de usuario:");
-                    String nombre = scanner.nextLine();
-                    System.out.println("Tu nombre es: " + nombre);
-                    System.out.println(separacion);
+                    verficarUser(scanner, user_credentials);
                     break;
                 case 2:
+                    System.out.println(user_credentials);
+                    System.out.println(separacion);
+                    break;
+                case 3:
                     System.out.println("Ceerrando programa...");
                     break;
                 default:
@@ -47,6 +46,30 @@ public class Test {
                     System.out.println(separacion);
                     break;
             } 
-        } while (option != 2);
+        } while (option != 3);
     } 
+    
+    public static void verficarUser(Scanner scanner, HashMap<String, String> user_credentials) {
+        System.out.println("Introduce nombre de usuario:");
+        System.out.println(separacion);
+        String name = scanner.nextLine(); 
+          
+        if (user_credentials.containsKey(name)) { // Verificar si el nombre está en la lista
+            System.out.println("Tu nombre es: " + name);
+            System.out.println("Introduce tu contraseña:");
+            System.out.println(separacion);
+            String pass = scanner.nextLine();
+            
+            if (user_credentials.get(name).equals(pass)) {
+                System.out.println("Inicio de sesion correcto.");
+                System.out.println(separacion);
+                 
+            } else {
+                System.out.println("Contraseña incorrecta");
+                System.out.println(separacion);
+            }
+        } else {
+            System.out.println("Nombre de usuario no válido.");
+        }
+    }
 }
